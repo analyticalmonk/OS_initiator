65862d91a8b3c4128a36bfa089156d6289c84080
Akash Tandon <akashtndn.acm@gmail.com>
2019-03-12 19:10:55 +0530

    updates ubuntu setup script

--- ubuntu/ubuntu_setup.sh
+++ ubuntu/ubuntu_setup.sh
@@ -4,36 +4,35 @@ sudo apt-get update && sudo apt-get upgrade
 # Install VIM
 sudo apt-get install vim
 
-# Install flux-gui (tool to make your computer's display adapt to time of the day)
+# Install redshift (tool to make your computer's display adapt to time of the day)
 # HIGHLY RECOMMENDED
-sudo add-apt-repository ppa:nathan-renniewaldock/flux
-sudo apt-get update && sudo apt-get install fluxgui
+sudo apt-get install redshift geoclue-2.0
+# You may need to turn on your `location services` to allow redshift to automatically adjust as per your location
 
 # Install ATOM text editor
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update && sudo apt-get install atom
 
 # Install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # Install OpenJDK 9 (Java)
-sudo apt-get update
-sudo apt-get install openjdk-9-jre-headless openjdk-9-jdk-headless
+# sudo apt-get install openjdk-9-jre-headless openjdk-9-jdk-headless
 
 # Install VLC media player
-sudo apt-get install vlc
+# sudo apt-get install vlc
 
 # Install Miniconda (package and environment manager for Python)
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 
 # Install R and RStudio (64-bit)
-sudo add-apt-repository ppa:marutter/rrutter
-sudo apt-get update && sudo apt-get install r-base
+sudo apt-get install libopenblas-base r-base
+
 # Download and install RStudio (might encounter dependency issues)
-wget https://download1.rstudio.org/rstudio-1.0.143-amd64.deb
-sudo dpkg -i rstudio-1.0.143-amd64.deb
+wget https://download1.rstudio.org/rstudio-1.1.463-amd64.deb
+sudo dpkg -i rstudio-1.1.463-amd64.deb
 sudo apt-get -f install
 
 # Install simplescreenrecorder
@@ -41,10 +40,10 @@ sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
 sudo apt update && sudo apt-get install simplescreenrecorder
 
 # Install mkvmerge (allows hardcoding subtitles into videos)
-sudo apt-get install mkvmerge
+# sudo apt-get install mkvmerge
 
 # Install TeX (texlive) - Uses ~ 3.5GB of disk space
-sudo apt-get install texlive-full
+# sudo apt-get install texlive-full
 
 # Install bumblebee (used when working with NVIDIA GPUs)
 # sudo apt-get install bumblebee primus
